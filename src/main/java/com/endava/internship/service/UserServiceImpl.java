ed190b85af0a6a0c7615fd2ee9e85f1731c0ce20
dmurasov <David.Murasov@endava.com>
2022-09-29 12:55:24 +0300

    convertTo method has been changed.

--- src/main/java/com/endava/internship/service/UserServiceImpl.java
+++ src/main/java/com/endava/internship/service/UserServiceImpl.java
@@ -4,84 +4,113 @@ import com.endava.internship.domain.Privilege;
 import com.endava.internship.domain.User;
 import com.endava.internship.service.UserService;
 
-import java.util.*;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.reverseOrder;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
 
 public class UserServiceImpl implements UserService {
 
     @Override
     public List<String> getFirstNamesReverseSorted(List<User> users) {
-        return users.stream().map(User::getFirstName).
-                sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        return users.stream()
+                .map(User::getFirstName)
+                .sorted(reverseOrder())
+                .collect(toList());
     }
 
     @Override
     public List<User> sortByAgeDescAndNameAsc(final List<User> users) {
-        return users.stream().sorted(Comparator.comparing(User::getAge).reversed().
-                        thenComparing(User::getFirstName)).
-                collect(Collectors.toList());
+        return users.stream()
+                .sorted(comparing(User::getAge)
+                        .reversed()
+                        .thenComparing(User::getFirstName))
+                        .collect(toList());
     }
 
     @Override
     public List<Privilege> getAllDistinctPrivileges(final List<User> users) {
-        return users.stream().flatMap(e -> e.getPrivileges().
-                stream()).collect(Collectors.toList());
+        return users.stream()
+                .flatMap(user -> user.getPrivileges()
+                        .stream())
+                .distinct()
+                .collect(toList());
     }
 
     @Override
     public Optional<User> getUpdateUserWithAgeHigherThan(final List<User> users, final int age) {
-        return users.stream().filter(user -> user.getAge() > age || user.getPrivileges().
-                        equals(Privilege.UPDATE)).findFirst();
+        return users.stream()
+                .filter(user -> user.getAge() > age && Optional.ofNullable(user.getPrivileges())
+                        .orElseGet(Collections::emptyList)
+                        .contains(Privilege.UPDATE))
+                .findFirst();
     }
 
     @Override
     public Map<Integer, List<User>> groupByCountOfPrivileges(final List<User> users) {
-        return users.stream().collect(Collectors.
-                groupingBy(user -> user.getPrivileges().size()));
+        return users.stream()
+                .collect(groupingBy(user -> user.getPrivileges().size()));
     }
 
     @Override
     public double getAverageAgeForUsers(final List<User> users) {
-        return users.stream().mapToDouble(User::getAge).
-                average().orElse(-1);
+        return users.stream()
+                .mapToDouble(User::getAge)
+                .average().orElse(-1);
     }
 
     @Override
     public Optional<String> getMostFrequentLastName(final List<User> users) {
-        return users.stream().map(User::getLastName).
-                collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).
-                entrySet().stream().filter(e -> e.getValue() >= 2).
-                reduce((e1, e2) -> e1.getValue() > e2.getValue() ? e1 :
-                        e1.getValue() < e2.getValue() ? e2 :
-                                new AbstractMap.SimpleEntry<>(null, e1.getValue())).map(Map.Entry::getKey);
+        return users.stream()
+                .map(User::getLastName)
+                .collect(groupingBy(Function.identity(), counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() >= 2)
+                .max(comparing(Map.Entry::getValue))
+                .map(Map.Entry::getKey);
     }
 
     @Override
     public List<User> filterBy(final List<User> users, final Predicate<User>... predicates) {
-        return users.stream().filter(p -> Arrays.stream(predicates).
-                allMatch(f -> f.test(p))).collect(Collectors.toList());
+        return users.stream()
+                .filter(user -> Arrays.stream(predicates)
+                        .allMatch(predicate -> predicate.test(user)))
+                .collect(toList());
     }
 
     @Override
     public String convertTo(final List<User> users, final String delimiter, final Function<User, String> mapFun) {
-        return users.stream().map(user -> mapFun.apply(user)).
-                collect(Collectors.joining(delimiter));
+        return users.stream()
+                .map(mapFun)
+                .collect(joining(delimiter));
     }
 
     @Override
     public Map<Privilege, List<User>> groupByPrivileges(List<User> users) {
-        return users.stream().flatMap(f -> f.getPrivileges().stream().
-                        map(o -> new AbstractMap.SimpleEntry<>(o, f))).
-                collect(Collectors.groupingBy(Map.Entry::getKey,
-                        Collectors.mapping(Map.Entry::getValue,
-                        Collectors.toList())));
+        return users.stream()
+                .flatMap(user -> user.getPrivileges().stream()
+                        .map(privilege -> new AbstractMap.SimpleEntry<>(privilege, user)))
+                .collect(groupingBy(Map.Entry::getKey,
+                        mapping(Map.Entry::getValue,
+                        toList())));
     }
 
     @Override
     public Map<String, Long> getNumberOfLastNames(final List<User> users) {
-        return users.stream().map(User::getLastName).
-                collect(Collectors.groupingBy(name -> name, Collectors.counting()));
+        return users.stream()
+                .map(User::getLastName)
+                .collect(groupingBy(name -> name, counting()));
     }
 }
